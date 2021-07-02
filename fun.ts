b1a67b48aebe2c06d70588307ec299ba385419c2
Atul Varma <varmaa@gmail.com>
2021-07-02 08:16:50 -0400

    Run the story.

--- fun.ts
+++ fun.ts
@@ -1,5 +1,7 @@
+import { assertNotNull } from "@justfixnyc/util/commonjs";
 import fs from "fs";
-import { Story } from "inkjs";
+import { Story } from "inkjs/engine/Story";
+import { ConsoleIO } from "./console-io";
 
 const rawText = fs
   .readFileSync("nycx.ink.json", { encoding: "utf-8" })
@@ -8,6 +10,44 @@ const rawText = fs
 
 const storyJson = JSON.parse(rawText);
 
-const story = new Story(storyJson);
+async function getChoiceIndex(story: Story, io: ConsoleIO): Promise<number> {
+  for (let choice of story.currentChoices) {
+    console.log(`${choice.index + 1}. ${choice.text}`);
+  }
+  while (true) {
+    const choice = await io.question("> ");
+    const index = parseInt(choice);
+    if (index > 0 && index <= story.currentChoices.length) {
+      return index - 1;
+    }
+    io.writeLine("Invalid choice!");
+  }
+}
 
-console.log(story.ContinueMaximally());
+async function main() {
+  const story = new Story(storyJson);
+  const io = new ConsoleIO();
+
+  story.onError = (msg, type) => {
+    console.error(msg, type);
+  };
+
+  while (true) {
+    if (story.canContinue) {
+      const message = story.Continue();
+      io.writeLine(assertNotNull(message));
+    } else if (story.currentChoices.length > 0) {
+      const choiceIdx = await getChoiceIndex(story, io);
+      story.ChooseChoiceIndex(choiceIdx);
+    } else {
+      break;
+    }
+  }
+
+  io.close();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
