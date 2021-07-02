aecd73f9190718c90126b868f943a5217ad8bd4e
Atul Varma <varmaa@gmail.com>
2021-07-02 07:23:13 -0400

    Add BOM comment.

--- fun.ts
+++ fun.ts
@@ -3,6 +3,7 @@ import { Story } from "inkjs";
 
 const rawText = fs
   .readFileSync("nycx.ink.json", { encoding: "utf-8" })
+  // Remove any BOM at the beginning.
   .replace(/^\uFEFF/, "");
 
 const storyJson = JSON.parse(rawText);
