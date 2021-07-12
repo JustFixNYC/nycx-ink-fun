3b50a840f7eee4a02d2b0f586f390534e7401429
Atul Varma <varmaa@gmail.com>
2021-07-12 10:42:30 -0400

    Refactor to make into a textbot more easily (#1)
    This changes the logic in `fun.ts` to make the conversation state easier to serialize, so that we can turn it into a textbot without too much effort.
    
    This effectively means that whenever we wait for user input, instead of using `await` on some kind of io-like object, we set some information in a conversation state object, serialize it, and then expect to be deserialized and called again if/when the input arrives.
    
    In addition to refactoring, this also squelches the echoing of the choice which the user made.

--- fun.ts
+++ fun.ts
@@ -9,6 +9,8 @@ import {
   validateHousingType,
 } from "./predict-housing-type";
 
+const INVALID_CHOICE_MSG = "Invalid choice!";
+
 const SPECIAL_INSTRUCTION_PREDICT_HOUSING_TYPE = ">>> PREDICT_HOUSING_TYPE";
 
 const PROMPT = "> ";
@@ -20,22 +22,10 @@ const rawText = fs
 
 const storyJson = JSON.parse(rawText);
 
-async function getChoiceIndex(story: Story, io: ConsoleIO): Promise<number> {
-  for (let choice of story.currentChoices) {
-    console.log(`${choice.index + 1}. ${choice.text}`);
-  }
-  while (true) {
-    const choice = await io.question(PROMPT);
-    const index = parseInt(choice);
-    if (index > 0 && index <= story.currentChoices.length) {
-      return index - 1;
-    }
-    io.writeLine("Invalid choice!");
-  }
-}
-
-async function askForAddressAndPredictHousingType(story: Story, io: ConsoleIO) {
-  const address = await io.question(PROMPT);
+async function getPredictedHousingTypeChoiceIdx(
+  story: Story,
+  input: string
+): Promise<number> {
   const choiceEntries = story.currentChoices.map(
     (choice) =>
       [validateHousingType(choice.text), choice.index] as [HousingType, number]
@@ -48,39 +38,136 @@ async function askForAddressAndPredictHousingType(story: Story, io: ConsoleIO) {
     }
   }
 
-  const housingType = await predictHousingType(address);
+  const housingType = await predictHousingType(input);
+  return assertNotUndefined(choiceMap.get(housingType));
+}
 
-  story.ChooseChoiceIndex(assertNotUndefined(choiceMap.get(housingType)));
+type SpecialInputMode = "predictHousingType";
+
+type ConversationState = {
+  storyState: any;
+  queuedInput: string[];
+  queuedOutput: string[];
+  specialInputMode: SpecialInputMode | null;
+  isWaitingForInput: boolean;
+  hasEnded: boolean;
+};
+
+function parseStoryChoice(story: Story, input: string): number | undefined {
+  const choiceInt = parseInt(input);
+  if (choiceInt > 0 && choiceInt <= story.currentChoices.length) {
+    return choiceInt - 1;
+  }
+}
+
+function getStoryChoicesMenu(story: Story): string {
+  return story.currentChoices
+    .map((choice) => `${choice.index + 1}. ${choice.text}`)
+    .join("\n");
+}
+
+async function processConversation(
+  cs: ConversationState,
+  story = new Story(storyJson)
+): Promise<ConversationState> {
+  let { specialInputMode, queuedInput, queuedOutput } = cs;
+  let didStoryContinue = false;
+  let didChoose = false;
+  const output = (message: string) => {
+    queuedOutput = [...queuedOutput, message];
+  };
+  const consumeInput = () => {
+    let input = null;
+    if (queuedInput.length > 0) {
+      input = queuedInput[0];
+      queuedInput = queuedInput.slice(1);
+    }
+    return input;
+  };
+  const choose = (idx: number) => {
+    story.ChooseChoiceIndex(idx);
+    didChoose = true;
+  };
+  story.state.LoadJson(JSON.stringify(cs.storyState));
+
+  if (story.canContinue) {
+    didStoryContinue = true;
+    const message = assertNotNull(story.Continue());
+    if (message.startsWith(SPECIAL_INSTRUCTION_PREDICT_HOUSING_TYPE)) {
+      specialInputMode = "predictHousingType";
+    } else {
+      output(message);
+    }
+  }
+
+  if (story.currentChoices.length > 0) {
+    if (specialInputMode === "predictHousingType") {
+      const input = consumeInput();
+      if (input) {
+        choose(await getPredictedHousingTypeChoiceIdx(story, input));
+        specialInputMode = null;
+      }
+    } else {
+      if (didStoryContinue) {
+        output(getStoryChoicesMenu(story));
+      }
+
+      const input = consumeInput();
+      if (input) {
+        const choiceIdx = parseStoryChoice(story, input);
+        if (choiceIdx !== undefined) {
+          choose(choiceIdx);
+        } else {
+          output(INVALID_CHOICE_MSG);
+        }
+      }
+    }
+  }
+
+  if (didChoose) {
+    // Ink always prints the choice the user made, which we don't want to do.
+    story.Continue();
+  }
+
+  return {
+    ...makeConversationState(story),
+    queuedInput,
+    queuedOutput,
+    specialInputMode,
+  };
+}
+
+function makeConversationState(story: Story): ConversationState {
+  return {
+    storyState: JSON.parse(story.state.ToJson()),
+    queuedInput: [],
+    queuedOutput: [],
+    specialInputMode: null,
+    isWaitingForInput: story.currentChoices.length > 0,
+    hasEnded: !story.canContinue && story.currentChoices.length === 0,
+  };
 }
 
 async function main() {
   const story = new Story(storyJson);
   const io = new ConsoleIO();
-  let specialInputMode: "predictHousingType" | null = null;
+  let convState = makeConversationState(story);
 
   story.onError = (msg, type) => {
     console.error(msg, type);
   };
 
-  while (true) {
-    if (story.canContinue) {
-      const message = assertNotNull(story.Continue());
-      if (message.startsWith(SPECIAL_INSTRUCTION_PREDICT_HOUSING_TYPE)) {
-        specialInputMode = "predictHousingType";
-      } else {
-        io.writeLine(message);
-      }
-    } else if (story.currentChoices.length > 0) {
-      if (specialInputMode === "predictHousingType") {
-        await askForAddressAndPredictHousingType(story, io);
-        specialInputMode = null;
-      } else {
-        const choiceIdx = await getChoiceIndex(story, io);
-        story.ChooseChoiceIndex(choiceIdx);
-      }
-    } else {
-      break;
+  while (!convState.hasEnded) {
+    for (let message of convState.queuedOutput) {
+      io.writeLine(message);
+      convState.queuedOutput = [];
     }
+
+    if (convState.isWaitingForInput) {
+      convState.queuedInput.push(await io.question(PROMPT));
+    }
+
+    convState = await processConversation(convState);
   }
 
   io.close();
