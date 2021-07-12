2ae02bdc71ac9734c3787ed5a75ac811ceaa2519
Atul Varma <varmaa@gmail.com>
2021-07-12 17:43:14 -0400

    Add body parser thing.

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import twilio from "twilio";
 import http from "http";
 import express from "express";
+import { urlencoded } from "body-parser";
+import MessagingResponse from "twilio/lib/twiml/MessagingResponse";
 
 dotenv.config();
 
@@ -12,6 +14,25 @@ const TWILIO_AUTH_TOKEN = getRequiredEnvVar("TWILIO_AUTH_TOKEN");
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 const app = express();
+
+app.use(urlencoded({ extended: false }));
+
+app.post("/sms", (req, res) => {
+  const body = req.body.Body;
+
+  if (body) {
+    const twiml = new MessagingResponse();
+
+    // TODO: Actually start/continue conversation.
+    twiml.message("BOOP");
+
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(twiml.toString());
+  } else {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("Body parameter required!");
+  }
+});
 
 http.createServer(app).listen(PORT, () => {
   console.log(`HTTP server listening on port ${PORT}.`);
