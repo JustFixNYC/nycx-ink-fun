8f9b999067e84ceb1379a274e9b088b2734f6b2a
Atul Varma <varmaa@gmail.com>
2021-07-13 06:35:55 -0400

    Create session data dir.

--- server.ts
+++ server.ts
@@ -1,15 +1,23 @@
 import dotenv from "dotenv";
 import twilio from "twilio";
 import http from "http";
+import path from "path";
 import express from "express";
 import { urlencoded } from "body-parser";
 import MessagingResponse from "twilio/lib/twiml/MessagingResponse";
+import { existsSync, mkdirSync } from "fs";
 
 dotenv.config();
 
 const PORT = process.env.PORT || "3000";
 const TWILIO_ACCOUNT_SID = getRequiredEnvVar("TWILIO_ACCOUNT_SID");
 const TWILIO_AUTH_TOKEN = getRequiredEnvVar("TWILIO_AUTH_TOKEN");
+
+const SESSION_DATA_DIR = path.join(__dirname, ".session-data");
+
+if (!existsSync(SESSION_DATA_DIR)) {
+  mkdirSync(SESSION_DATA_DIR);
+}
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
